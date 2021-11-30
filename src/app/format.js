4b830b4bcc5950a3a1b16d84669f8c084f376f49
Samuel32600 <samuel.parmentier@orange.fr>
2021-11-30 11:04:33 +0100

    correction format date

--- src/app/format.js
+++ src/app/format.js
@@ -7,6 +7,7 @@ export const formatDate = (dateStr) => {
   const date = new Date(dateStr)
   const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
   const mo = new Intl.DateTimeFormat('fr', { month: 'numeric' }).format(date)
+  const da = new Intl.DateTimeFormat('fr', { day: 'numeric' }).format(date)
 
   // const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
   // const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
@@ -14,7 +15,7 @@ export const formatDate = (dateStr) => {
   // const month = mo.charAt(0).toUpperCase() + mo.slice(1)
   // return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
   
-  return `${ye}-${mo}-${da}`
+  return `${da}-${mo}-${ye}`
 }
  
 export const formatStatus = (status) => {
