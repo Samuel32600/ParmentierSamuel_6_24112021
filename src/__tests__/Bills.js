90e8d10a650b7d6856e7903dc0aead91b1be827f
Samuel32600 <samuel.parmentier@orange.fr>
2021-12-01 15:16:36 +0100

    test for loading page

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,22 +4,32 @@ import { bills } from "../fixtures/bills.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+
     test("Then bill icon in vertical layout should be highlighted", () => {
       const html = BillsUI({ data: []})
       document.body.innerHTML = html
       //to-do write expect expression
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      
       // Bug report - Bills
       // dates.sort(antiChrono)
-      
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
+    //-------------------------------
+    //-----test for Loading Page-----
+    //-------------------------------
+    test("Then control if loading page is correctly loaded", () => {
+      const html = BillsUI({ loading: true });
+      document.body.innerHTML = html;
+      expect(screen.getAllByText("Loading...")).toBeTruthy();
+    })
+
   })
 })
