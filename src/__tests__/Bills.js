03ab81545b1ad67525c504263fb5c1a061df9076
Samuel32600 <samuel.parmentier@orange.fr>
2021-12-03 09:59:15 +0100

    test for handleClickIconEye

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,12 +1,17 @@
-import { screen } from "@testing-library/dom"
+import { screen, fireEvent } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+
+import { ROUTES } from "../constants/routes"
+import Bills from "../containers/Bills.js"
+
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
 
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
+      const html = BillsUI({ data: [] })
       document.body.innerHTML = html
       //to-do write expect expression
     })
@@ -16,7 +21,7 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = html
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      // Bug report - Bills
+      // -----Bug report - Bills-----
       // dates.sort(antiChrono)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
@@ -37,10 +42,38 @@ describe("Given I am connected as an employee", () => {
     test("Then, it should render ErrorPage", () => {
       const html = BillsUI({ error: true });
       document.body.innerHTML = html;
-      expect(screen.getAllByText('Error')).toBeTruthy()
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
     })
 
+    //-------------------------------------
+    //-----test for handleClickIconEye-----
+    //-------------------------------------
+    describe("When I click on the icon eye", () => {
+      test("Then, preview should open", () => {
+        
+        const html = BillsUI({ data: bills })
+        document.body.innerHTML = html
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const bill = new Bills({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage
+        })
+        $.fn.modal = jest.fn()
+        const handleClickIconEye = jest.fn(() => bill.handleClickIconEye)
+        const eye = screen.queryAllByTestId('icon-eye')
+        eye[0].addEventListener("click", handleClickIconEye)
+        fireEvent.click(eye[0])
+        expect(handleClickIconEye).toHaveBeenCalled()
+        const modale = screen.getByTestId("modaleFileEmployee")
+        expect(modale).toBeTruthy()
+      })
+    })
+    
   })
 })
 
-//-----test for handleClickIconEye-----
+
