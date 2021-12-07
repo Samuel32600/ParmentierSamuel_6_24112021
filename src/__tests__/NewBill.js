0e5f202189968f1551c749688435b39b59a5bfc0
Samuel32600 <samuel.parmentier@orange.fr>
2021-12-07 15:28:50 +0100

    test sur icone mail actif sur new bills

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -12,77 +12,91 @@ import firebase from "../__mocks__/firebase"
 
 
 describe("Given I am connected as an employee", () => {
-  describe("When I download a file with a good extention", () => {
-    test("the submit button is available", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then mail icon in vertical navbar should be highlighted", () => {
+      Object.defineProperty(window, "localStorage", { value: localStorageMock })
+      window.localStorage.setItem("user", JSON.stringify({
+        type: "Employee"
+      }))
       const html = NewBillUI()
       document.body.innerHTML = html
+      const iconActive = screen.getByTestId("icon-mail")
+      expect(iconActive.classList.contains("active-icon")).toBeTruthy
+    })
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
+    describe("When I download a file with a good extention", () => {
+      test("the submit button is available", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
 
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
-      const firestore = null
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        firestore,
-        localStorage: window.localStorage
-      })
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
 
-      const inputData = new File(["filetest"], 'filetest.jpeg', { type: "image/jpeg" })
-      const filetest = screen.getByTestId("file")
-      const btnSend = document.getElementById("btn-send-bill")
-      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
-
-      filetest.addEventListener("change", handleChangeFile)
-      fireEvent.change(filetest, {
-        target: {
-          files: [inputData],
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
         }
+        const firestore = null
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore,
+          localStorage: window.localStorage
+        })
+
+        const inputData = new File(["filetest"], 'filetest.jpeg', { type: "image/jpeg" })
+        const filetest = screen.getByTestId("file")
+        const btnSend = document.getElementById("btn-send-bill")
+        const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
+
+        filetest.addEventListener("change", handleChangeFile)
+        fireEvent.change(filetest, {
+          target: {
+            files: [inputData],
+          }
+        })
+        expect(handleChangeFile).toHaveBeenCalled()
+        expect(btnSend.disabled).not.toBeTruthy()
       })
-      expect(handleChangeFile).toHaveBeenCalled()
-      expect(btnSend.disabled).not.toBeTruthy()
-    })
-  })
-})
-
-describe("When I download a file with bad extention", () => {
-  test("the submit button is not clikable ", () => {
-    const html = NewBillUI()
-    document.body.innerHTML = html
-
-    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-    window.localStorage.setItem('user', JSON.stringify({
-      type: 'Employee'
-    }))
-
-    const onNavigate = (pathname) => {
-      document.body.innerHTML = ROUTES({ pathname })
-    }
-    const firestore = null
-    const newBill = new NewBill({
-      document,
-      onNavigate,
-      firestore,
-      localStorage: window.localStorage
     })
 
-    const inputData = new File(["filetest"], 'filetest.txt', { type: "image/txt" })
-    const filetest = screen.getByTestId("file")
-    const btnSend = document.getElementById("btn-send-bill")
-    const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
 
-    filetest.addEventListener("change", handleChangeFile)
-    fireEvent.change(filetest, {
-      target: {
-        files: [inputData],
-      }
+    describe("When I download a file with bad extention", () => {
+      test("the submit button is not clikable ", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const firestore = null
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore,
+          localStorage: window.localStorage
+        })
+
+        const inputData = new File(["filetest"], 'filetest.txt', { type: "image/txt" })
+        const filetest = screen.getByTestId("file")
+        const btnSend = document.getElementById("btn-send-bill")
+        const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
+
+        filetest.addEventListener("change", handleChangeFile)
+        fireEvent.change(filetest, {
+          target: {
+            files: [inputData],
+          }
+        })
+        expect(handleChangeFile).toHaveBeenCalled()
+        expect(btnSend.disabled).toBeTruthy()
+      })
     })
-    expect(handleChangeFile).toHaveBeenCalled()
-    expect(btnSend.disabled).toBeTruthy()
   })
 })
